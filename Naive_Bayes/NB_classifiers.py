28ff7fa6cd5b61abda9fe8b4a7fd694f4e4caea2
Daniel Yang <daniel.yj.yang@gmail.com>
2020-08-29 19:29:53 -0500

    Update

--- Naive_Bayes/NB_classifiers.py
+++ Naive_Bayes/NB_classifiers.py
@@ -6,6 +6,10 @@ Created on Sat Aug 29 15:32:23 2020
 @author: daniel
 """
 
+######################################################################################################
+
+# Multinomial Naive Bayes classifier
+
 # https://stackabuse.com/the-naive-bayes-algorithm-in-python-with-scikit-learn/
 
 import pandas as pd
@@ -68,3 +72,33 @@ print(np.mean(predicted == y_test))
 from sklearn.metrics import confusion_matrix
 
 print(confusion_matrix(y_test, predicted))
+
+######################################################################################################
+
+# Gaussian Naive Bayes classifier
+
+# https://www.geeksforgeeks.org/naive-bayes-classifiers/
+
+# load the iris dataset
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+# store the feature matrix (X) and response vector (y)
+X = iris.data
+y = iris.target
+
+# splitting X and y into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+
+# training the model on training set
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+# making predictions on the testing set
+y_pred = gnb.predict(X_test)
+
+# comparing actual response values (y_test) with predicted response values (y_pred)
+from sklearn import metrics
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
