3d6c96e32fd35b3a7aaa5b99f895b77e128391e6
Daniel Yang <daniel.yj.yang@gmail.com>
2020-08-31 19:21:23 -0500

    Update

--- model_training_and_optimization/batch_gradient_descent/logistic_regression_two_features.py
+++ model_training_and_optimization/batch_gradient_descent/logistic_regression_two_features.py
@@ -6,12 +6,14 @@ Created on Wed Feb 21 21:35:32 2018
 @author: Daniel Yang, Ph.D. (daniel.yj.yang@gmail.com)
 """
 
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 import numpy as np
-import sklearn
-from sklearn import datasets
+from sklearn import datasets, linear_model
 from sklearn.metrics import accuracy_score
+from statsmodels.discrete.discrete_model import Logit
+from statsmodels.tools import add_constant
 
 plt.rcParams['animation.ffmpeg_path'] = '/usr/local/bin/ffmpeg'
 plt.rcParams['animation.convert_path'] = '/usr/local/bin/convert'
@@ -20,9 +22,16 @@ plt.rcParams.update({'font.size': 20})
 # equivalent to rcParams['animation.html'] = 'html5'
 rc('animation', html='html5')
 
-iris = datasets.load_iris()
-X = iris.data[:, :2]
-y = (iris.target != 0) * 1
+# Source: https://towardsdatascience.com/understanding-logistic-regression-step-by-step-704a78be7e0a
+data = pd.read_csv('/Users/daniel/Data-Science/Data/Gender/01_heights_weights_genders.csv')
+y = data['Gender']
+X = data[['Height','Weight']]
+
+y = y.map({'Female': 0, 'Male': 1})
+
+#iris = datasets.load_iris()
+#X = iris.data[:, :2]
+#y = (iris.target != 0) * 1
 
 # modified but a very early version was based on https://medium.com/@martinpella/logistic-regression-from-scratch-in-python-124c5636b8ac
 
@@ -94,6 +103,24 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
     def predict(self, X, threshold=0.5):
         return self.predict_prob(X) >= threshold
 
+    # this result is different from statsmodels, but statsmodels is correct
+    def sklearn_Logistic_Regression(self, X, y):
+        LR_model = linear_model.LogisticRegression(
+            fit_intercept=True, C=1e9)
+        LR_model.fit(X, y)
+        print(LR_model.intercept_, LR_model.coef_)
+        #predicted_classes = LR_model.predict(X)
+        #accuracy = accuracy_score(y.flatten(), predicted_classes)
+        #print(accuracy)
+        return LR_model
+
+    def statsmodels_Logit(self, X, y):
+        model = Logit(endog=y, exog=add_constant(X))
+        results = model.fit(maxiter=10000)
+        #print(results.summary())
+        #print(results.summary2())
+        return results # see results.params.values
+
     def plot_decision_boundary(self, epoch=None):
 
         # references:
@@ -101,7 +128,7 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
         # https://towardsdatascience.com/building-a-logistic-regression-in-python-301d27367c24
 
         # plot the original data
-        plt.figure(figsize=(8, 8))
+        plt.figure(figsize=(10, 10))
         plt.scatter(self.X[:, 1], self.X[:, 2], marker="o", c=self.y)
 
         # the decision boundary
@@ -117,8 +144,18 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
         X1_values = [np.min(self.X[:, 1])-0.1, np.max(self.X[:, 1])+0.1]
         X2_values = (-self.theta[0] -
                      np.dot(self.theta[1], X1_values)) / self.theta[2]
+
         plt.plot(X1_values, X2_values, color='red',
-                 linestyle='dashed', label='Decision Boundary')
+                 linestyle='dashed', label='BGD decision boundary')
+
+        LR_model_statsmodel = self.statsmodels_Logit(self.X, self.y)
+        params_estimates = LR_model_statsmodel.params.values
+        X2_values_Logit = (-params_estimates[0] -
+                     np.dot( params_estimates[1], X1_values)) / params_estimates[2]
+
+        plt.plot(X1_values, X2_values_Logit, color='blue',
+                 linestyle='dashed', label='statsmodels Logit solution')
+
         plt.xlabel('X1')
         plt.ylabel('X2')
         plt.legend()
@@ -126,7 +163,7 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
 
     def plot_loss_history(self):
         # construct a figure that plots the loss over time
-        plt.figure(figsize=(8, 8))
+        plt.figure(figsize=(10, 10))
         plt.plot(np.arange(0, len(self.training_History)), np.array(
             self.training_History)[:, 0], label='Training Loss')
         plt.legend(loc=1)
@@ -136,10 +173,16 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
 
     # see also https://xavierbourretsicotte.github.io/animation_ridge.html
     def animate_decision_boundary(self):
+
+        LR_model_statsmodel = self.statsmodels_Logit(self.X, self.y)
+        params_estimates = LR_model_statsmodel.params.values
+
         # First set up the figure, the axis, and the plot element we want to animate
-        fig1, ax1 = plt.subplots(figsize=(8, 8))
-        line, = ax1.plot([], [], color='red', linestyle='dashed',
+        fig1, ax1 = plt.subplots(figsize=(10, 10))
+        line1, = ax1.plot([], [], color='red', linestyle='dashed',
                          label='Decision Boundary', lw=1.5)
+        line2, = ax1.plot([], [], color='blue', linestyle='dashed',
+                          label='statsmodels Logit solution', lw=1.5)
         point, = ax1.plot([], [], '*', color='red', markersize=4)
         value_display = ax1.text(0.02, 0.02, '', transform=ax1.transAxes)
         # https://stackoverflow.com/questions/9401658/how-to-animate-a-scatter-plot?rq=1
@@ -148,12 +191,13 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
         plt.ylabel('X2')
 
         def init_1():
-            line.set_data([], [])
+            line1.set_data([], [])
+            line2.set_data([], [])
             point.set_data([], [])
             value_display.set_text('')
             scatterplot.set_offsets([])
 
-            return line, point, value_display, scatterplot
+            return line1, line2, point, value_display, scatterplot
 
         def update_1(epoch):
             # Animate line
@@ -162,7 +206,12 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
             # print(this_theta)
             X2_values = (-this_theta[0] -
                          np.dot(this_theta[1], X1_values)) / this_theta[2]
-            line.set_data(X1_values, X2_values)
+            line1.set_data(X1_values, X2_values)
+
+            X2_values_Logit= (-params_estimates[0] -
+                     np.dot( params_estimates[1], X1_values)) / params_estimates[2]
+            line2.set_data(X1_values, X2_values_Logit)
+
             #print(epoch, X1_values, X2_values)
 
             # Animate points
@@ -177,12 +226,10 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
 
             # https://stackoverflow.com/questions/43674917/animation-in-matplotlib-with-scatter-and-using-set-offsets-autoscale-of-figure
             # scale
-            ax1.set_xlim(min(min(self.X[:, 1]), min(
-                X1_values))-0.1, max(max(self.X[:, 1]), max(X1_values))+0.1)
-            ax1.set_ylim(min(min(self.X[:, 2]), min(
-                X2_values))-0.1, max(max(self.X[:, 2]), max(X2_values))+0.1)
+            ax1.set_xlim(     np.min(self.X[:, 1])                   -0.1, np.max(self.X[:, 1])+0.1 )
+            ax1.set_ylim( min(np.min(self.X[:, 2]),np.min(X2_values))-0.1, np.max(self.X[:, 2])+0.1 )
 
-            return line, point, value_display, scatterplot
+            return line1, line2, point, value_display, scatterplot
 
         ax1.legend(loc="upper left")
 
@@ -201,11 +248,13 @@ class Logistic_regression_as_optimized_by_batch_gradient_descent:
 # 1. Batch gradient descent
 # https://medium.com/@martinpella/logistic-regression-from-scratch-in-python-124c5636b8ac
 model = Logistic_regression_as_optimized_by_batch_gradient_descent(
-    lr=0.2, num_iter=1000)
+    lr=0.00025, num_iter=1000)
 model.fit(X, y)
+
 model.animate_decision_boundary()
 
 model.plot_decision_boundary()
+
 model.plot_loss_history()
 
 print(model.theta)
@@ -213,15 +262,12 @@ predicted_classes = model.predict(X)
 accuracy = accuracy_score(y.flatten(), predicted_classes)
 print(accuracy)
 
+# 2. statsmodels
+LR_results = model.statsmodels_Logit(X, y)
 
-# 2. Sklearn
-model = sklearn.linear_model.LogisticRegression(
-    fit_intercept=True, C=1e20, penalty='none')
-model.fit(X, y)
-print(model.intercept_, model.coef_)
-predicted_classes = model.predict(X)
-accuracy = accuracy_score(y.flatten(), predicted_classes)
-print(accuracy)
+
+# 3. Sklearn
+#LR_solution = model.sklearn_Logistic_Regression(X, y)
 
 
 # next1: penalty
