1ccf3c4d33bf78846d9a08d21f43d04a374104c5
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-05 09:31:38 -0500

    Update PCA.R

--- PCA/PCA.R
+++ PCA/PCA.R
@@ -1,3 +1,5 @@
+# Daniel Yang, Ph.D. (daniel.yj.yang@gmail.com)
+
 # https://www.datacamp.com/community/tutorials/pca-analysis-r
 
 require(datasets)
@@ -14,6 +16,7 @@ ggbiplot(iris.pca, ellipse=TRUE, groups=iris$Species, obs.scale = 1, var.scale =
 require(factoextra)
 fviz_eig(iris.pca)
 fviz_eig(iris.pca, choice = "eigenvalue", geom = "line", addlabels = TRUE, xlab = "Principal Component" )
+fviz_eig(iris.pca, choice = "variance", geom = "bar", addlabels = TRUE, xlab = "Principal Component" )
 fviz_pca_var(iris.pca,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
