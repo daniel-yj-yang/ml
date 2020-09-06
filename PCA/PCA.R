50c3f9e4d9e8f7bf89fb278488c2843be3c3619e
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-05 23:42:41 -0500

    Update

--- PCA/PCA.R
+++ PCA/PCA.R
@@ -9,17 +9,26 @@ iris.pca <- prcomp(iris[,c(1:4)], center = TRUE, scale. = TRUE)
 
 summary(iris.pca)
 
-require(magrittr)
-iris.pca$rotation ^2 %>% colSums() # the eigenvector is normalized to have magnitude/length of 1
+eigenvalues <- iris.pca$sdev^2
+eigenvectors <- iris.pca$rotation
 
+sum(eigenvalues)
+require(magrittr)
+eigenvectors ^2 %>% colSums() # the eigenvector is normalized to have magnitude/length of 1
+
+# biplot
 require(ggbiplot)
 ggbiplot(iris.pca, ellipse=TRUE, groups=iris$Species, obs.scale = 1, var.scale = 1)
 
 # http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/
 require(factoextra)
 fviz_eig(iris.pca)
+
+# scree plot
 fviz_eig(iris.pca, choice = "eigenvalue", geom = "line", addlabels = TRUE, xlab = "Principal Component" )
 fviz_eig(iris.pca, choice = "variance", geom = "bar", addlabels = TRUE, xlab = "Principal Component" )
+
+# loading plot
 fviz_pca_var(iris.pca,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
