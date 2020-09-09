887ec13a2feb4d210b9cfc01f847a6eae2746f9e
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-09 11:21:38 -0500

    Update

---
+++ PCA/images/PCA_2PCs.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 10:01:51 2020
+
+@author: daniel
+"""
+
+# generate variables
+from numpy import mean
+from numpy import std
+from numpy.random import randn
+from numpy.random import seed
+from matplotlib import pyplot as plt
+# seed random number generator
+seed(1230293)
+# prepare data
+n = 10000
+var1 = 20 * randn(n) + 50
+var2 = var1 + (10 * randn(n) + 20)
+# summarize
+print('variable1: mean=%.3f stdv=%.3f' % (mean(var1), std(var1)))
+print('variable2: mean=%.3f stdv=%.3f' % (mean(var2), std(var2)))
+# plot
+#create a new figure
+plt.figure(figsize=(7,7))
+plt.scatter(var1, var2)
+plt.show()
+
+# PCA
+import pandas as pd
+from numpy.linalg import svd
+from numpy import transpose
+
+X_raw = pd.DataFrame({'var1': var1, 'var2': var2})
+X_colMeans = mean(X_raw, axis = 0)
+#X_colStds = std(X_raw, axis=0)
+X = (X_raw - X_colMeans) #/ X_colStds
+U, D, V_t = svd(X, full_matrices=False)
+W = transpose(V_t) # eigenvectors
+L = D**2 / (n-1) # eigenvalues
+
+#create a new figure
+plt.figure(figsize=(7,7))
+plt.scatter(X_raw['var1'], X_raw['var2'])
+plt.axis('equal')
+
+slope_PC1 = W[1,0]/W[0,0] # a vector pointing toward (W[0,0], W[1,0])
+intercept_PC1 = X_colMeans[1] - X_colMeans[0]* slope_PC1
+
+plt.plot([ min(X_raw['var1']),                           max(X_raw['var1'])                           ],
+         [ intercept_PC1 + slope_PC1*min(X_raw['var1']), intercept_PC1 + slope_PC1*max(X_raw['var1']) ],
+         'r')
+
+slope_PC2 = W[1,1]/W[0,1] # a vector pointing toward (W[0,1], W[1,1])
+intercept_PC2 = X_colMeans[1] - X_colMeans[0]* slope_PC2
+
+plt.plot([ min(X_raw['var1']),                           max(X_raw['var1'])                           ],
+         [ intercept_PC2 + slope_PC2*min(X_raw['var1']), intercept_PC2 + slope_PC2*max(X_raw['var1']) ],
+         'r')
+
+plt.show()
+
+# PCA
+from sklearn.decomposition import PCA
+import numpy as np
+pca = PCA(n_components=2)
+PCs = pca.fit_transform(X)
+pca.components_ # eigenvectors
+pca.explained_variance_ # eigenvalues
+#create a new figure
+plt.figure(figsize=(7,7))
+plt.scatter(PCs[:,0], PCs[:,1])
+plt.axis('equal')
+plt.plot([ np.min(PCs), np.max(PCs) ],
+         [ 0,             0   ],
+         'r')
+plt.plot([ 0,             0   ],
+         [ np.min(PCs), np.max(PCs) ],
+         'r')
+plt.show()
